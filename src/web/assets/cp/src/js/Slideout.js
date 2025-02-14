e2588081d29b4c2eb3a23fb8aec7a5af8eabdd72
Lupe Camacho <lupe@pixelandtonic.com>
2025-02-14 14:14:11 -0800

    Remove commented code

--- src/web/assets/cp/src/js/Slideout.js
+++ src/web/assets/cp/src/js/Slideout.js
@@ -270,7 +270,6 @@ import $ from 'jquery';
         }
       },
       removePanel: function (panel) {
-        const totalPanels = Craft.Slideout.totalPanels();
         Craft.Slideout.openPanels = Craft.Slideout.openPanels.filter(
           (m) => m !== panel
         );
@@ -280,10 +279,6 @@ import $ from 'jquery';
           panel.$container.css(Garnish.ltr ? 'left' : 'right', '100vw');
           Craft.Slideout.updateStyles();
         }
-
-        // if (Craft.Slideout.totalPanels() === 0) {
-        //   panel.$outerContainer.css('display', 'none');
-        // }
       },
       updateStyles: function () {
         const totalPanels = Craft.Slideout.totalPanels();
