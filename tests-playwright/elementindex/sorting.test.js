824c1288fba1e12b92a73ad4a1f95229c9e1efda
i-just <iwona@pixelandtonic.com>
2025-02-17 16:03:43 +0000

    cleanup

--- tests-playwright/elementindex/sorting.test.js
+++ tests-playwright/elementindex/sorting.test.js
@@ -10,42 +10,61 @@ test.beforeEach(async ({page}) => {
   await page.goto('./entries/testSorting');
 });
 
+test('Check customizing view', async ({page, baseURL}) => {
+
+  const viewBtnSelector = '#toolbar button:has-text("View")'
+  const viewBtn = await page.waitForSelector(viewBtnSelector);
+  await page.click(viewBtnSelector);
+
+  let menuId = await viewBtn.getAttribute('aria-controls');
+  menuId = menuId.replace('.', '\\.');
+
+  let cols = [
+    "Number Field",
+    "Plain Text Field",
+    "Entry Type",
+    "ID",
+  ];
+
+  for (let i = 0; i < cols.length; i++) {
+    const itemSelector = '#' + menuId + ' .table-columns-field label:has-text("' + cols[i] + '")';
+    await page.waitForSelector(itemSelector);
+    await page.click(itemSelector);
+  }
+
+  const closeSelector = '#' + menuId + ' button:has-text("Close")';
+  await page.waitForSelector(closeSelector);
+  await page.click(closeSelector);
+
+  for (let i = 0; i < cols.length; i++) {
+    await expect(page.locator('.elements table.data thead')).toContainText(cols[i]);
+  }
+
+});
+
 test.describe('Sorting', () => {
-  test('Check customizing view', async ({page, baseURL}) => {
 
-    const viewBtnSelector = '#toolbar button:has-text("View")'
-    const viewBtn = await page.waitForSelector(viewBtnSelector);
-    await page.click(viewBtnSelector);
-
-    let menuId = await viewBtn.getAttribute('aria-controls');
-    menuId = menuId.replace('.', '\\.');
-
-    let cols = [
-      "Number Field",
-      "Plain Text Field",
-      "Entry Type",
-      "ID",
-    ];
-
-    for (let i = 0; i < cols.length; i++) {
-      const itemSelector = '#' + menuId + ' .table-columns-field label:has-text("' + cols[i] + '")';
-      await page.waitForSelector(itemSelector);
-      await page.click(itemSelector);
-    }
-
-    const closeSelector = '#' + menuId + ' button:has-text("Close")';
-    await page.waitForSelector(closeSelector);
-    await page.click(closeSelector);
-
-    for (let i = 0; i < cols.length; i++) {
-      await expect(page.locator('.elements table.data thead')).toContainText(cols[i]);
-    }
-
-  });
+  const viewBtnSelector = '#toolbar button:has-text("View")'
+  let cols = [
+    {
+      field: "Number Field",
+      firstTitle: "Test sorting 1",
+      lastTitle: "Test sorting 10",
+    },
+    {
+      field: "Plain Text Field",
+      firstTitle: "Test sorting 1",
+      lastTitle: "Test sorting 3",
+    },
+    {
+      field: "ID",
+      firstTitle: "Test sorting 1",
+      lastTitle: "Test sorting 10",
+    },
+  ];
 
   test('Check sort asc', async ({page, baseURL}) => {
 
-    const viewBtnSelector = '#toolbar button:has-text("View")'
     const viewBtn = await page.waitForSelector(viewBtnSelector);
     await page.click(viewBtnSelector);
 
@@ -56,24 +75,6 @@ test.describe('Sorting', () => {
     const sortAscendingSelector = '#' + menuId + ' section button[data-dir="asc"]';
     const sortDescendingSelector = '#' + menuId + ' section button[data-dir="desc"]';
     const closeSelector = '#' + menuId + ' button:has-text("Close")';
-
-    let cols = [
-      {
-        field: "Number Field",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 10",
-      },
-      {
-        field: "Plain Text Field",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 3",
-      },
-      {
-        field: "ID",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 10",
-      },
-    ];
 
     // test sort ascending
     for (let i = 0; i < cols.length; i++) {
@@ -87,7 +88,6 @@ test.describe('Sorting', () => {
 
   test('Check sort desc', async ({page, baseURL}) => {
 
-    const viewBtnSelector = '#toolbar button:has-text("View")'
     const viewBtn = await page.waitForSelector(viewBtnSelector);
     await page.click(viewBtnSelector);
 
@@ -99,25 +99,7 @@ test.describe('Sorting', () => {
     const sortDescendingSelector = '#' + menuId + ' section button[data-dir="desc"]';
     const closeSelector = '#' + menuId + ' button:has-text("Close")';
 
-    let cols = [
-      {
-        field: "Number Field",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 10",
-      },
-      {
-        field: "Plain Text Field",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 3",
-      },
-      {
-        field: "ID",
-        firstTitle: "Test sorting 1",
-        lastTitle: "Test sorting 10",
-      },
-    ];
-
-    // test sort ascending
+    // test sort descending
     for (let i = 0; i < cols.length; i++) {
       await page.locator(sortDropdownSelector).selectOption({'label': cols[i].field});
       await page.click(sortAscendingSelector);
