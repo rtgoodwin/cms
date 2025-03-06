a843895a24efeee645b43c76c91d055b3fe7c29e
Brian Hanson <brian@brianhanson.net>
2025-03-06 11:26:29 -0600

    Accessibility updates

--- src/web/assets/graphiql/src/components/SchemaSelector.jsx
+++ src/web/assets/graphiql/src/components/SchemaSelector.jsx
@@ -1,34 +1,17 @@
-import React, {useRef} from 'react';
-
-function setSchema(uid) {
-  const url = new URL(window.location.href);
-  url.searchParams.set('schemaUid', uid);
-
-  location.href = url.toString();
-}
+import React from 'react';
 
 export const SchemaSelector = ({items = [], value = ''}) => {
-  /**
-   * @type {React.MutableRefObject<null|HTMLFormElement>}
-   */
-  const form = useRef(null);
-
-  function handleChange() {
-    form.current?.requestSubmit();
-  }
-
   return (
-    <div className="schema-selector">
-      <form method="GET" ref={form}>
-        <div className="input">
+    <form method="GET" className="schema-selector">
+      <div className="field">
+        <div className="heading">
+          <label htmlFor="schemaUid">Select Schema</label>
+        </div>
+        <div className="input ltr">
           <div className="select">
-            <label htmlFor="schemaUid" className="visually-hidden">
-              GraphQL Schema
-            </label>
             <select
               name="schemaUid"
               className="select"
-              onChange={handleChange}
               defaultValue={value}
             >
               {Object.keys(items).map((key, index) => {
@@ -41,7 +24,11 @@ export const SchemaSelector = ({items = [], value = ''}) => {
             </select>
           </div>
         </div>
-      </form>
-    </div>
+      </div>
+
+      <button className="btn" type="submit">
+        Go
+      </button>
+    </form>
   );
 };
