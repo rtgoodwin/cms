8d46ce1256fdaf5ca7ca907d8f1ed8b446d9d953
brandonkelly <brandon@pixelandtonic.com>
2025-03-18 15:49:23 -0700

    Use getDraftElementId()

--- src/web/assets/cp/src/js/NestedElementManager.js
+++ src/web/assets/cp/src/js/NestedElementManager.js
@@ -481,25 +481,12 @@ Craft.NestedElementManager = Garnish.Base.extend(
       }
     },
 
-    getElementOwnerId($element) {
-      let ownerId = null;
-      let elementEditor = this.elementEditor;
-      if (typeof elementEditor == 'undefined') {
-        elementEditor = $element.closest('form').data('elementEditor');
-      }
-      if (
-        elementEditor &&
-        $element.data('owner-id') !== elementEditor.settings.elementId
-      ) {
-        ownerId = elementEditor.settings.elementId;
-      }
-
-      return ownerId;
-    },
-
     createElementEditor($element) {
+      const elementOwnerId = $element.data('owner-id');
       const slideout = Craft.createElementEditor(this.elementType, $element, {
-        ownerId: this.getElementOwnerId($element),
+        ownerId:
+          this.elementEditor?.getDraftElementId(elementOwnerId) ||
+          elementOwnerId,
         onLoad: () => {
           slideout.elementEditor.on('update', () => {
             Craft.Preview.refresh();
