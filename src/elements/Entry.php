f7f967aa25faf8dc4377b4e7942f5b3d738db85c
brandonkelly <brandon@pixelandtonic.com>
2025-03-31 05:23:12 -0700

    Render entry section attributes as chips

--- src/elements/Entry.php
+++ src/elements/Entry.php
@@ -2109,18 +2109,19 @@ class Entry extends Element implements NestedElementInterface, ExpirableElementI
                 return $html;
             case 'section':
                 $section = $this->getSection();
-                return $section ? Html::encode(Craft::t('site', $section->name)) : '';
+                if (!$section) {
+                    return '';
+                }
+                return Cp::chipHtml($section, [
+                    'class' => 'chromeless',
+                    'showThumb' => false,
+                ]);
             case 'type':
                 try {
-                    $config = [
-                        'attributes' => [
-                            'class' => ['chromeless'],
-                        ],
-                    ];
-                    if ($this->viewMode === 'cards') {
-                        $config['showThumb'] = false;
-                    }
-                    return Cp::chipHtml($this->getType(), $config);
+                    return Cp::chipHtml($this->getType(), [
+                        'class' => 'chromeless',
+                        'showThumb' => $this->viewMode !== 'cards',
+                    ]);
                 } catch (InvalidConfigException) {
                     return Craft::t('app', 'Unknown');
                 }
