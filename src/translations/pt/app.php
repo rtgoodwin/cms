b4738a17aa0990e53a22584c9c9fe565e4caf84b
shinybrad <83238715+shinybrad@users.noreply.github.com>
2025-04-10 20:55:33 -0400

    New translations app.php (Portuguese) [ci skip]

--- src/translations/pt/app.php
+++ src/translations/pt/app.php
@@ -649,7 +649,7 @@ return [
     'Emerald' => 'Esmeralda',
     'Emirate' => 'Emirato',
     'Enable focal point' => 'Ativar ponto de foco',
-    'Enable versioning for entries in this section' => 'Ativar o controle de versão para as entradas nesta seção',
+    'Enable versioning for entries in this section' => 'Ativar o controlo de versão para as entradas neste campo',
     'Enable' => 'Ativar',
     'Enabled for all sites' => 'Ativado para todos os sites',
     'Enabled' => 'Habilitado',
