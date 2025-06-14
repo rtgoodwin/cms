ccbca66eef8537782840c9b8be1ad4732c574eac
brandonkelly <brandon@pixelandtonic.com>
2025-06-14 22:41:50 +1000

    Translation cleanup

--- src/translations/en/app.php
+++ src/translations/en/app.php
@@ -2309,7 +2309,6 @@ return [
     '“{handle}” is a reserved word.' => '“{handle}” is a reserved word.',
     '“{handle}” isn’t a valid handle.' => '“{handle}” isn’t a valid handle.',
     '“{name}” deleted.' => '“{name}” deleted.',
-    'Ungrouped' => 'Ungrouped',
     'Add Group' => 'Add Group',
     'Move to the next group' => 'Move to the next group',
     'Move to the previous group' => 'Move to the previous group',
